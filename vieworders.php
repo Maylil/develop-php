51873950a9808d751597cefe943b34851399709e
Maylil <may.li@sagesoft.cn>
2014-01-10 18:00:46 +0800

    2014.1.10

--- vieworders.php
+++ vieworders.php
@@ -14,10 +14,29 @@
 		echo '<p><strong>No orders pending.Please try again later.</strong></p>';
 		exit;
 	}
+	echo 'file sizes: '.filesize($DOCUMENT_ROOT.'/../orders/orders.txt').'<br/>';
 	while (!feof($fp)){
 		$order = fgets($fp,999);
 		echo $order.'<br/>';
 	}
+	$price = array('Tires'=>100,'oil'=>10,'Spank Plugs'=>4);
+	foreach ($price as $key => $value){
+		echo $key.'-'.$value.'<br/>';
+	}
+// 	while($element = each($price)){
+// // 		echo $element['key'].'-'.$element['value'].'<br/>';
+// 			echo $element['key'];  
+// 　　	echo ' - ';
+// 　　	echo $element['value'];
+// 　　	echo '<br/>';
+// 	}
+
+// 	while(list($product,$price) = each($price)){
+// 		echo "$product - $price<br/>";
+// 	}
+	$a = array('a','b','c','d');
+	$b = array('a','k');
+	var_dump($a + $b);
 ?>
 </body>
 </html>
